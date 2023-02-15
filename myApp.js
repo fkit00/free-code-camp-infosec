a1884c5027966024811f10796160ffa71ee94745
fiona <fiona.kitchen@yahoo.co.uk>
2023-02-15 13:28:02 +0000

    refactored

--- myApp.js
+++ myApp.js
@@ -3,23 +3,23 @@ const app = express();
 const helmet = require("helmet")
 
 
-let ninetyDaysInSeconds= 90*24*60*60
-
-app.use(helmet.hidePoweredBy())
-app.use(helmet.frameguard({action:"deny"})) // this stops people putting your site in an aframe and using it for clickjacking
-app.use(helmet.xssFilter())
-
-app.use(helmet.noSniff())
-// this is used to reduce MIME sniffing
-app.use(helmet.ieNoOpen())
-app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force:true}))
-//asking users to avoid insecure http
-app.use(helmet.dnsPrefetchControl()) // this results in high security but performance can go down
 app.use(helmet.noCache())
 
 //content security policiy- prevent injection by allowing a list of content 
 app.use(helmet.contentSecurityPolicy({directives:{defaultSrc:["'self'"], scriptSrc:["'self'", "trusted-cdn.com"]}}))
 
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}))
 
 
 
